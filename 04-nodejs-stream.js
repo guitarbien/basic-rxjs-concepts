060fc71849114d6dc6127436ea081c161b3b014a
guitarbien <guitarbien@gmail.com>
2018-01-01 20:50:30 +0800

    將 giveMeSomeData() 參數改為傳入一個物件
    把三個 method 封裝成物件 observer 然後傳入

--- 04-nodejs-stream.js
+++ 04-nodejs-stream.js
@@ -1,25 +1,22 @@
-function nextCallback(data) {
-    console.log(data);
+function giveMeSomeData(ob) {
+    // document.addEventListener('click', ob.next); // ob.next is MouseEvent, 'click' won't fail
+    // fetch(url).then(ob.next, ob.error);
+    [10, 20, 30].forEach(ob.next);
+    ob.complete();
 }
 
-function errorCallback(err) {
-    console.log(err);
-}
-
-function completeCallback() {
-    console.log('done');
-}
-
-function giveMeSomeData(nextCB, errorCB, completeCB) {
-    // document.addEventListener('click', nextCB); // nextCB is MouseEvent, 'click' won't fail
-    // fetch(url).then(nextCB, errorCB);
-    // [10, 20, 30].forEach(nextCB)
-}
+const observer = {
+    next: function nextCallback(data) {
+        console.log(data);
+    },
+    error: function errorCallback(err) {
+        console.log(err);
+    },
+    complete: function completeCallback() {
+        console.log('done');
+    }
+};
 
 // 以 async 方式取得資料時，將資料留給 callback 處理
 // const data = giveMeSomeData( ... );
-giveMeSomeData(
-    nextCallback,
-    errorCallback,
-    completeCallback
-);
+giveMeSomeData(observer);
