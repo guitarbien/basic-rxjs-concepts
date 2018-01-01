b04b837327e23c4bab484393e693c339f1e0f30d
guitarbien <guitarbien@gmail.com>
2018-01-01 21:06:18 +0800

    (formatting code)

--- 04-nodejs-stream.js
+++ 04-nodejs-stream.js
@@ -1,28 +1,28 @@
 // mouse event
 const clickObservable = {
-    subscribe: function (ob) {
-        document.addEventListener('click', ob.next);
-    }
+  subscribe: function (ob) {
+    document.addEventListener('click', ob.next);
+  }
 };
 
 // array
 const arrayObservable = {
-    subscribe: function (ob) {
-        [10, 20, 30].forEach(ob.next);
-        ob.complete();
-    }
+  subscribe: function (ob) {
+    [10, 20, 30].forEach(ob.next);
+    ob.complete();
+  }
 };
 
 const observer = {
-    next: function nextCallback(data) {
-        console.log(data);
-    },
-    error: function errorCallback(err) {
-        console.log(err);
-    },
-    complete: function completeCallback() {
-        console.log('done');
-    }
+  next: function nextCallback(data) {
+    console.log(data);
+  },
+  error: function errorCallback(err) {
+    console.log(err);
+  },
+  complete: function completeCallback() {
+    console.log('done');
+  }
 };
 
 // 以 async 方式取得資料時，將資料留給 callback 處理
